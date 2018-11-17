967bbd4b3d824e9290aca9af721333cc4420b868
zuzannazuzewicz <zuzanna.zuzewicz@gmail.com>
2018-11-17 15:58:31 +0100

    Expanded app, added possibility of adding todos to local storage and update, and delete todo functionalities

--- src/app/todo-item/todo-item.component.ts
+++ src/app/todo-item/todo-item.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { TodoItem} from '../interfaces/todo-item';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { TodoItem } from '../interfaces/todo-item';
 
 @Component({
   selector: 'app-todo-item',
@@ -8,10 +8,23 @@ import { TodoItem} from '../interfaces/todo-item';
 })
 export class TodoItemComponent implements OnInit {
   @Input() item: TodoItem;
+  @Output() remove = new EventEmitter<TodoItem>();
+  @Output() update = new EventEmitter<any>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  removeItem() {
+    this.remove.emit(this.item);
+  }
+
+  completeItem() {
+    this.update.emit({
+      item: this.item,
+      changes: {completed: !this.item.completed}
+    });
   }
 
 }

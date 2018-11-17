0574ccd4a901568fc2f77b434d43ffdc394a02f4
zuzannazuzewicz <zuzanna.zuzewicz@gmail.com>
2018-11-17 14:28:32 +0100

    Added basic app functionalities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TodoItem } from './interfaces/todo-item';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-list';
+  title = 'app';
+  todoList: TodoItem[] = [
+    {title: 'install NodeJS'},
+    {title: 'install Angular CLI'},
+    {title: 'create new app'},
+    {title: 'serve app'},
+    {title: 'develop app'},
+    {title: 'deploy app'},
+  ];
+
+  addItem(title: string) {
+    this.todoList.push({ title });
+  }
 }
